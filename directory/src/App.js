5a960dbf09b0662f848875e52e19c018b79b0f9c
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-07-11 22:42:23 -0400

    updated file structure

--- directory/src/App.js
+++ directory/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import EmployeeCard from "./Components/EmployeeCard";
+import API from "./utils/API"
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  // Setting this.state.employees to the employees json array
+  state = {
+    search: "",
+    results: []
+  };
 
-    </div>
-  );
+  // Map over this.state.employees and render an EmployeeCard component for each employee object
+  render() {
+    return (
+      <div>  {this.state.employees.map(employee => (
+        <EmployeeCard
+          name={`${employee.name.first} ${employee.name.last}`} />
+      ))}
+      </div>
+    )
+  }
+
 }
 
 export default App;
--- directory/src/utils/API.js
+++ directory/src/utils/API.js
@@ -5,6 +5,7 @@ const BASEURL = "https://randomuser.me/api/?results=300&nat=us";
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
     search: function (query) {
+        console.log(axios.get(BASEURL));
         return axios.get(BASEURL);
     }
 };
